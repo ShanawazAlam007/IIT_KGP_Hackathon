f88e7f2a532405d16168e2d3ac7787d5fe34911e
ShanawazAlam007 <22052755@kiit.ac.in>
2025-03-14 01:09:28 +0530

    Set up Express server with user authentication and transaction routes

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require('path');
+const cors = require("cors"); 
+const user_Route = require("./routes/user");
+const user_transaction = require("./routes/user_transactions");
+
+const app = express();
+const port = process.env.port || 8080;
+
+
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/auth", user_Route);
+app.use("/api",user_transaction);
+
+// Error middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Something went wrong!" });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is hosted on port ${port}`);
+});
